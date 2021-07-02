4853c620ccaadf0e4ffb271cd5d8cc3454667e3b
mpheath <mpheath@tpg.com.au>
2021-07-03 04:38:01 +1000

    Added download and copy Luacheck to base folder.

--- download_base_tools.py
+++ download_base_tools.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-'''Download and extract base tools from the zip files.'''
+'''Copy or extract base tools from the downloaded files.'''
 
-import os, urllib.request, zipfile
+import os, shutil, urllib.request, zipfile
 import common
 
 
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     os.chdir(download)
 
-    # Dictionary. key (folder|bit), url to download and dest as extract root.
+    # Dictionary. key 'folder|bit', url to download and dest as copy|extract root.
     dic = {
         'eskil': {
             'url': 'http://eskil.tcl-lang.org/index.html/uv/htdocs/download/'
@@ -35,46 +35,72 @@ if __name__ == '__main__':
         'frhed|x64': {
             'url': 'https://github.com/WinMerge/frhed/releases/download/'
                    '0.10904.2017/frhed-0.10904.2017.7-x64.zip',
-            'dest': base}}
+            'dest': base},
+        'luacheck|x64': {
+            'url': 'https://github.com/mpeterv/luacheck/releases/download'
+                   '/0.23.0/luacheck.exe',
+            'dest': os.path.join(base, 'lua')}}
 
-    # Check base tools folders exist.
+    # Check base tools files and subfolders exist.
     for key in dic:
-        if not os.path.isdir(os.path.join(base, key.split('|')[0])):
+        if dic[key]['url'].endswith('.exe'):
+            fullpath = os.path.join(dic[key]['dest'],
+                                    os.path.basename(dic[key]['url']))
+
+            if not os.path.isfile(fullpath):
+                break
+
+        elif not os.path.isdir(os.path.join(base, key.split('|')[0])):
             break
     else:
-        print('All base tools subfolders already exist\ndone')
+        print('All base tools files and subfolders already exist\ndone')
         exit()
 
-    # Download and extract from the zip file.
+    # Download, copy or extract each file as needed.
     for key, items in dic.items():
         title, bit = key.split('|', 1) if '|' in key else (key, '')
         folder = os.path.join(base, title)
         url = items['url']
         dest = items['dest']
+        title = title.title()
+        file = os.path.basename(url)
+        prompt = '' if os.path.isfile(file) else 'Download and '
 
-        if os.path.isdir(folder):
-            continue
-        else:
-            title = title.title()
+        # Ask the user if OK to do the operation.
+        if url.endswith('.exe'):
+            fullpath = os.path.join(dest, file)
 
-            reply = input('Download and extract {}? [n|y]: '
-                          .format((title + ' ' + bit) if bit else title))
-
-            if reply.lower() != 'y':
+            if os.path.isfile(fullpath):
                 continue
 
-            os.makedirs(folder)
+            prompt += ('copy' if prompt else 'Copy') + ' {}? [n|y]: '
+        elif os.path.isdir(folder):
+            continue
+        else:
+            prompt += ('extract' if prompt else 'Extract') + ' {}? [n|y]: '
 
-        file = os.path.basename(url)
+        reply = input(prompt.format((title + ' ' + bit) if bit else title))
 
+        if reply.lower() != 'y':
+            continue
+
+        # Download the file.
         try:
             if not os.path.isfile(file):
                 urllib.request.urlretrieve(url, file)
         except urllib.error.HTTPError as err_msg:
             print('   ', err_msg)
-            status = False
         else:
-            with zipfile.ZipFile(file) as z:
-                z.extractall(dest)
+            # Extract if a zip file else copy the file to base.
+            if file.endswith('.zip'):
+                os.makedirs(folder)
+
+                with zipfile.ZipFile(file) as z:
+                    z.extractall(dest)
+            else:
+                if not os.path.exists(dest):
+                    os.makedirs(dest)
+
+                shutil.copyfile(file, fullpath)
 
     print('done')
