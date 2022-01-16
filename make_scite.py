8aefdd0293e279c3f29e5bb170e9e0f3257ba463
mpheath <mpheath@tpg.com.au>
2022-01-16 12:44:54 +1000

    Added Make TestLexer to the menu of Make SciTE.

--- make_scite.py
+++ make_scite.py
@@ -5,7 +5,7 @@
 import glob, os, subprocess
 
 
-def make():
+def make_scite():
     '''Make SciTE.exe and Sc1.exe.'''
 
     cwd = os.getcwd()
@@ -19,6 +19,23 @@ def make():
     # Common paths.
     paths.append(os.path.join(cwd, 'scintilla', 'win32'))
     paths.append(os.path.join(cwd, 'scite', 'win32'))
+
+    # Compile.
+    for path in paths:
+        with subprocess.Popen('mingw32-make', cwd=path) as p:
+            p.wait()
+
+
+def make_testlexer():
+    '''Make TestLexer.exe for testing Lexilla lexers.'''
+
+    cwd = os.getcwd()
+
+    # SciTE 5.0 and later has a separate lexilla folder.
+    if os.path.isdir('lexilla'):
+        paths = [os.path.join(cwd, 'lexilla', 'test')]
+    else:
+        paths = [os.path.join(cwd, 'scintilla', 'lexilla', 'test')]
 
     # Compile.
     for path in paths:
@@ -56,14 +73,21 @@ if __name__ == '__main__':
     print('Make SciTE\n'
           '----------\n'
           ' 0  Quit\n'
-          ' 1  Make\n'
-          ' 2  Clean\n')
+          ' 1  Make SciTE\n'
+          ' 2  Make TestLexer\n'
+          ' 3  Clean\n')
 
-    reply = input('Enter a number: ')
+    reply = input('Enter numbers: ').strip()
 
-    if reply == '1':
-        make()
-    elif reply == '2':
-        clean()
+    if reply:
+        for item in reply.split():
+            if item == '1':
+                make_scite()
+            elif item == '2':
+                make_testlexer()
+            elif item == '3':
+                clean()
+            else:
+                print(item, 'is an invalid number')
 
     print('done')
